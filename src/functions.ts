aed331f279f29a86323b3d9f0c932e8cd10ddc2e
Leo Acord <leoacord@udel.edu>
2026-02-18 13:09:09 -0500

    Completed convertYesNo

--- src/functions.ts
+++ src/functions.ts
@@ -50,5 +50,9 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    return (
+        word.toLowerCase() === "yes" ? true
+        : word.toLowerCase() === "no" ? false
+        : null
+    );
 }
