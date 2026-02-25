4262f2db90c22d2d6155f6e2de378d6c0b9e6943
Leo Acord <leoacord@udel.edu>
2026-02-25 12:57:25 -0500

    completed toShortForm

--- src/objects.ts
+++ src/objects.ts
@@ -58,7 +58,9 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const stringRep =
+        `${question.id}: ${question.name.substring(0, 10)}`.toString();
+    return stringRep;
 }
 
 /**
