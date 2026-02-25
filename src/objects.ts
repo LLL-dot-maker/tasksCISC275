5aeea6839bb2115da01ad712c9157a873fae8ae3
Leo Acord <leoacord@udel.edu>
2026-02-25 12:47:24 -0500

    Completed isValid

--- src/objects.ts
+++ src/objects.ts
@@ -44,7 +44,11 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    const questionType: boolean =
+        question.type === `short_answer_question` ?
+            true
+        :   question.options.includes(answer);
+    return questionType;
 }
 
 /**
