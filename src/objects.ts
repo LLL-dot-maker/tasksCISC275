b5b06c482ebf4e25ae82e84b643d1251b76a2b49
Leo Acord <leoacord@udel.edu>
2026-02-25 13:52:13 -0500

    Completed upto publishQuestion

--- src/objects.ts
+++ src/objects.ts
@@ -108,7 +108,9 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    let invertedPublishing: boolean = !question.published;
+    const newQuestion = { ...question, published: invertedPublishing };
+    return newQuestion;
 }
 
 /**
