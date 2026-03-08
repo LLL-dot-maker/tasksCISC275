61e9a2f2f6f7726e7181264fd0388e09f90a041d
LLL-dot-maker <leo.acord@gmail.com>
2026-03-08 12:38:05 -0400

    Completed counter and reveal answer

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
+// import { StartAttempt } from "./components/StartAttempt";
+// import { TwoDice } from "./components/TwoDice";
+// import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
 
 function App(): React.JSX.Element {
@@ -17,14 +17,14 @@ function App(): React.JSX.Element {
             <Counter></Counter>
             <hr />
             <RevealAnswer></RevealAnswer>
-            <hr />
+            {/* <hr />
             <StartAttempt></StartAttempt>
             <hr />
-            <TwoDice></TwoDice>
+            <TwoDice></TwoDice> */}
             <hr />
             <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
+            {/* <hr />
+            <CycleHoliday></CycleHoliday> */}
         </div>
     );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,17 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, setVisible] = useState<boolean>(true);
+
+    function flipVisibility(): void {
+        setVisible(!visible);
+    }
+
+    return (
+        //The answer to life and the universe
+        <div>
+            <Button onClick={flipVisibility}>Reveal Answer</Button>
+            {visible && <div>42</div>}
+        </div>
+    );
 }
