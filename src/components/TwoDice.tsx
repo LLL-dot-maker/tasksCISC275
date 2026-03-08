e261ddf3ea853b073ba7ff0fe746b43e905a0cd6
LLL-dot-maker <leo.acord@gmail.com>
2026-03-08 18:19:31 -0400

    Finished changeType, startAttempt, twoDice

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,31 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setleftDie] = useState<number>(1);
+    const [rightDie, setRightDie] = useState<number>(2);
+
+    const gameOver: boolean = leftDie === rightDie;
+    const snakeEyes: boolean = leftDie === rightDie && leftDie === 1;
+    return (
+        <div>
+            <span data-testid="left-die">{leftDie}</span>
+            <span data-testid="right-die">{rightDie}</span>
+            <br></br>
+            <Button
+                onClick={() => {
+                    setleftDie(d6);
+                }}
+            >
+                Roll Left
+            </Button>
+            <Button
+                onClick={() => {
+                    setRightDie(d6);
+                }}
+            >
+                Roll Right
+            </Button>
+            <div>{gameOver && <span>{snakeEyes ? "Lose" : "Win"}</span>}</div>
+        </div>
+    );
 }
