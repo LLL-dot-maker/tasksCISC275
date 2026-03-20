c8c55019798de8ef06bd3021192649fc334c0bfc
LLL-dot-maker <leo.acord@gmail.com>
2026-03-20 12:41:58 -0400

    Completed CheckAnswer, working on GiveAttempts

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,30 +1,21 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
-    const [answer, setAnswer] = useState<string>("");
-
-    function UpdateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
-        setAnswer(event.target.value);
-    }
-
-    function AnswerCheck(): string {
-        if (answer === expectedAnswer) {
-            return "✔️";
-        } else {
-            return "❌";
-        }
-    }
+    const [givenAnswer, setGivenAnswer] = useState<string>("");
     return (
         <div>
             <h3>Check Answer</h3>
-            <Form.Control value={answer} onChange={UpdateAnswer} />
-            <Button onClick={AnswerCheck}>Check Answer </Button>
-            {AnswerCheck()}
+            <input
+                value={givenAnswer}
+                onChange={(event) => {
+                    setGivenAnswer(event.target.value);
+                }}
+            ></input>
+            <span>{givenAnswer === expectedAnswer ? "✔️" : "❌"}</span>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attemptsLeft, setAttemptsLeft] = useState<number>(3);
+    const [attemptsRequested, setAttemptsRequested] = useState<string>("");
     return (
         <div>
             <h3>Give Attempts</h3>
+            <p>Attempts Left: {attemptsLeft}</p>
+            <input
+                type={"number"}
+                value={attemptsRequested}
+                onChange={(event) => {
+                    setAttemptsRequested(event.target.value);
+                }}
+            ></input>
         </div>
     );
 }
